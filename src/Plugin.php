97c01dafda3536d1327d7644ad238140a068ca46
yamunaprasath <yamunaprasath@softsolutions4u.info>
2022-08-12 10:34:11 +0530

    initial commit

---
+++ src/Plugin.php
@@ -0,0 +1,12 @@
+<?php
+namespace craft\commerce\postfinance;
+
+class Plugin extends \craft\base\Plugin
+{
+    public function init()
+    {
+        parent::init();
+
+        // Custom initialization code goes here...
+    }
+}
